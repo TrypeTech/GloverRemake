d472fc56d818294d3e4052a14bd9927818f1870a
stevo <steveharroldart3d@gmail.com>
2019-09-07 12:05:51 -0700

    Added Character Controller
    added third person stuff

---
+++ Project/Glover-Remake/Assets/Exported/Scripts/BlockSides.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSides : MonoBehaviour
+{
+
+    // set player into push state when enter trigger
+    // set player out of Aniamtio state when exit trigger
+
+    public enum BlockSide
+    {
+        Forward,
+        Backward,
+        Left,
+        Right
+    }
+    public BlockSide side;
+    Pushable pushable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pushable = gameObject.GetComponentInParent<Pushable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+           
+            pushable.canPush = true;
+            pushable.anim.SetBool("Pushing", true);
+            if(side == BlockSide.Forward)
+            {
+                pushable.moveDirection = -Vector3.right;
+            }
+            if (side == BlockSide.Backward)
+            {
+                pushable.moveDirection = Vector3.right;
+            }
+
+            if (side == BlockSide.Left)
+            {
+                pushable.moveDirection = -Vector3.forward;
+            }
+            if (side == BlockSide.Right)
+            {
+                pushable.moveDirection = Vector3.forward;
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            pushable.canPush = false;
+            pushable.anim.SetBool("Pushing", false);
+        }
+    }
+}
